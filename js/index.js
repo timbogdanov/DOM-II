672b6d12c044436f85a5797277f96974a1706efa
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-06-02 13:18:49 -0700

    add 3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// Your code goes here
+const bus = document.querySelector('header .img');
+
+bus.addEventListener('click', enlargeImg);
+
+function enlargeImg(event) {
+  bus.classList.add('enlarge-img');
+}
+
+document.addEventListener('keydown', closeOnEsc);
+
+function closeOnEsc(event) {
+  if (event.key === 'Escape') {
+    bus.classList.remove('enlarge-img');
+  }
+}
+
+
+
+const introP = document.querySelector('.intro p');
+
+introP.addEventListener('mouseover', enlargeText);
+
+function enlargeText(event) {
+  introP.style.fontSize = '2.5rem';
+  introP.style.fontFamily = 'monospace';
+}
