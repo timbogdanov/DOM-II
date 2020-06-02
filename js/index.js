7ddbfe8ffa0a65aebb00062f6008831267d72aa3
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-06-02 14:24:58 -0700

    add 6 event listeners

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Change image sizes and key down
 const bus = document.querySelector('header .img');
 
 bus.addEventListener('click', enlargeImg);
@@ -15,12 +16,43 @@ function closeOnEsc(event) {
 }
 
 
-
+// Change paragraph text sizes
 const introP = document.querySelector('.intro p');
 
 introP.addEventListener('mouseover', enlargeText);
+introP.addEventListener('mouseout', normalText);
 
 function enlargeText(event) {
   introP.style.fontSize = '2.5rem';
   introP.style.fontFamily = 'monospace';
 }
+
+function normalText(event) {
+  introP.style.fontSize = '1.6em';
+  introP.style.fontFamily = 'sans-serif';
+}
+
+
+// Change image opacities
+const imgContent = document.querySelectorAll('img');
+
+imgContent.forEach(item => {
+  item.addEventListener('dblclick', opacity);
+
+  function opacity(event) {
+    item.style.opacity = "0.1";
+  }
+});
+
+
+// Show window size of browser
+
+const height = document.querySelector('#height');
+const width = document.querySelector('#width');
+
+function reportWindowSize() {
+  height.textContent = window.innerHeight;
+  width.textContent = window.innerWidth;
+}
+
+window.onresize = reportWindowSize;
