6dbc77564395724df1f9b18f566664fd243d76d6
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-06-02 15:29:24 -0700

    finish 10 event listeners

--- js/index.js
+++ js/index.js
@@ -1,52 +1,42 @@
 // Change image sizes and key down
 const bus = document.querySelector('header .img');
 
-bus.addEventListener('click', enlargeImg);
-
-function enlargeImg(event) {
+bus.addEventListener('click', enlargeImg => {
   bus.classList.add('enlarge-img');
-}
+});
 
-document.addEventListener('keydown', closeOnEsc);
-
-function closeOnEsc(event) {
+document.addEventListener('keydown', closeOnEsc => {
   if (event.key === 'Escape') {
     bus.classList.remove('enlarge-img');
   }
-}
+});
 
 
 // Change paragraph text sizes
 const introP = document.querySelector('.intro p');
 
-introP.addEventListener('mouseover', enlargeText);
-introP.addEventListener('mouseout', normalText);
-
-function enlargeText(event) {
+introP.addEventListener('mouseover', enlargeText => {
   introP.style.fontSize = '2.5rem';
   introP.style.fontFamily = 'monospace';
-}
+});
 
-function normalText(event) {
+introP.addEventListener('mouseout', normalText => {
   introP.style.fontSize = '1.6em';
   introP.style.fontFamily = 'sans-serif';
-}
+});
 
 
 // Change image opacities
 const imgContent = document.querySelectorAll('img');
 
 imgContent.forEach(item => {
-  item.addEventListener('dblclick', opacity);
-
-  function opacity(event) {
+  item.addEventListener('dblclick', opacity => {
     item.style.opacity = "0.1";
-  }
+  });
 });
 
 
 // Show window size of browser
-
 const height = document.querySelector('#height');
 const width = document.querySelector('#width');
 
@@ -56,3 +46,32 @@ function reportWindowSize() {
 }
 
 window.onresize = reportWindowSize;
+
+
+// keyup
+
+const destinationBtn = document.querySelector('.destination .btn');
+document.addEventListener('keyup', expandText => {
+  if (event.key === 'a') {
+    destinationBtn.style.backgroundColor = 'salmon';
+  }
+});
+
+// mousedown mouseup
+
+const destinationBtn2 = document.querySelector('.destination:nth-of-type(2) .btn');
+destinationBtn2.addEventListener('mousedown', transform => {
+  destinationBtn2.style.transform = 'scale(1.5)';
+});
+
+destinationBtn2.addEventListener('mouseup', transformNormal => {
+  destinationBtn2.style.transform = 'scale(1)';
+});
+
+
+// contextmenu
+
+const destinationBtn3 = document.querySelector('.destination:nth-of-type(3) .btn');
+destinationBtn3.addEventListener('contextmenu', rightClick => {
+  destinationBtn3.style.backgroundColor = 'salmon';
+});
